bfb2e21ba2120fd3d6b4d267ea2ce7edc9f4d1b8
rafsan-jany <rafsan.mbstu@gmail.com>
2024-10-13 18:50:48 +0600

    stored user data in usersContext for global access

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
 import Users from './components/Users';
 import NewUser from './components/NewUser';
-import { UsersContext } from './context/UsersContext';
+import UsersProvider, { UsersContext } from './context/UsersContext';
 
 function App() {
 
-  const[users, setUsers] = useState([
-    {id: 1, username: 'anisul'},
-    {id: 2, username: 'rafsan'},
-  ])
-
   return (
-    <UsersContext.Provider value= {{users, setUsers}}>
+    <UsersProvider>
       <div className="App">
         <NewUser/>
         <Users/>
       </div>
-    </UsersContext.Provider>
-
+    </UsersProvider>
   );
 }
 
--- src/context/UsersContext.js
+++ src/context/UsersContext.js
@@ -1,6 +1,23 @@
-import { createContext, useContext } from "react";
+import { createContext, useState } from "react";
+import React from "react";
 
 export const UsersContext = createContext({});
+
+const UsersProvider = ({children}) => {
+    const[users, setUsers] = useState([
+        {id: 1, username: 'anisul'},
+        {id: 2, username: 'rafsan'},
+      ]) 
+
+    return  <UsersContext.Provider value= {{users, setUsers}}>
+        {children}
+    </UsersContext.Provider>
+}
+
+export default UsersProvider
+
+
+
 
 // 1. create useContext
 // 2. provide the useContext
